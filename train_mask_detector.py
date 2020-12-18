640139ee64ef8f211a692f1729e3d901c317f5c5
Oleg Lukanin <oleglukanin1@gmail.com>
2020-12-18 13:30:46 +0100

    Add files via upload

--- train_mask_detector.py
+++ train_mask_detector.py
@@ -1,4 +1,4 @@
-# import the necessary packages
+# Importing the necessary packages
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.applications import MobileNetV2
 from tensorflow.keras.layers import AveragePooling2D
@@ -20,17 +20,15 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 
-# initialize the initial learning rate, number of epochs to train for,
-# and batch size
+# Initializing the learning rate, number of epochs, and batch size
 INIT_LR = 1e-4
 EPOCHS = 20
 BS = 32
 
-DIRECTORY = r"C:\Mask Detection\CODE\Face-Mask-Detection-master\dataset"
+DIRECTORY = r"dataset"
 CATEGORIES = ["with_mask", "without_mask"]
 
-# grab the list of images in our dataset directory, then initialize
-# the list of data (i.e., images) and class images
+# Taking images from our dataset and adding them to the list of our data
 print("[INFO] loading images...")
 
 data = []
@@ -47,7 +45,7 @@ for category in CATEGORIES:
     	data.append(image)
     	labels.append(category)
 
-# perform one-hot encoding on the labels
+# Performing one-hot categorical encoding on our data labels
 lb = LabelBinarizer()
 labels = lb.fit_transform(labels)
 labels = to_categorical(labels)
@@ -55,10 +53,11 @@ labels = to_categorical(labels)
 data = np.array(data, dtype="float32")
 labels = np.array(labels)
 
+# Splitting the data for training and testing
 (trainX, testX, trainY, testY) = train_test_split(data, labels,
 	test_size=0.20, stratify=labels, random_state=42)
 
-# construct the training image generator for data augmentation
+# Construct the training image generator so the model understands the data
 aug = ImageDataGenerator(
 	rotation_range=20,
 	zoom_range=0.15,
@@ -68,13 +67,11 @@ aug = ImageDataGenerator(
 	horizontal_flip=True,
 	fill_mode="nearest")
 
-# load the MobileNetV2 network, ensuring the head FC layer sets are
-# left off
+# Load the MobileNetV2 architecture, ensuring the top layer is not fully connected
 baseModel = MobileNetV2(weights="imagenet", include_top=False,
 	input_tensor=Input(shape=(224, 224, 3)))
 
-# construct the head of the model that will be placed on top of the
-# the base model
+# Constructing the top layer of the model to be placed on top of the the base model
 headModel = baseModel.output
 headModel = AveragePooling2D(pool_size=(7, 7))(headModel)
 headModel = Flatten(name="flatten")(headModel)
@@ -82,22 +79,21 @@ headModel = Dense(128, activation="relu")(headModel)
 headModel = Dropout(0.5)(headModel)
 headModel = Dense(2, activation="softmax")(headModel)
 
-# place the head FC model on top of the base model (this will become
-# the actual model we will train)
+# Connecting the base model to the output (top) layer
+# This model is the final model to be trained with our datasets
 model = Model(inputs=baseModel.input, outputs=headModel)
 
-# loop over all layers in the base model and freeze them so they will
-# *not* be updated during the first training process
+# Making sure the weights in the base model layers are not updated with training
 for layer in baseModel.layers:
 	layer.trainable = False
 
-# compile our model
+# Compiling the model
 print("[INFO] compiling model...")
 opt = Adam(lr=INIT_LR, decay=INIT_LR / EPOCHS)
 model.compile(loss="binary_crossentropy", optimizer=opt,
 	metrics=["accuracy"])
 
-# train the head of the network
+# Training the output layer
 print("[INFO] training head...")
 H = model.fit(
 	aug.flow(trainX, trainY, batch_size=BS),
@@ -106,23 +102,22 @@ H = model.fit(
 	validation_steps=len(testX) // BS,
 	epochs=EPOCHS)
 
-# make predictions on the testing set
+# Making prediction on the test data
 print("[INFO] evaluating network...")
 predIdxs = model.predict(testX, batch_size=BS)
 
-# for each image in the testing set we need to find the index of the
-# label with corresponding largest predicted probability
+# Finding the index of a label with largest predicted probability for each image
 predIdxs = np.argmax(predIdxs, axis=1)
 
-# show a nicely formatted classification report
+# Creating a report
 print(classification_report(testY.argmax(axis=1), predIdxs,
 	target_names=lb.classes_))
 
-# serialize the model to disk
+# Saving the model
 print("[INFO] saving mask detector model...")
 model.save("mask_detector.model", save_format="h5")
 
-# plot the training loss and accuracy
+# Plotting training/validation loss and accuracy
 N = EPOCHS
 plt.style.use("ggplot")
 plt.figure()
